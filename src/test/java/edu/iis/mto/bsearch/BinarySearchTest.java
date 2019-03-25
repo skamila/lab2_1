359d28e611c284d5698d1451f9715e2c9c715721
struszczak.kamila <209472@edu.p.lodz.pl>
2019-03-25 22:50:11 +0100

    testy_v2 zmiana struktury testów jednostkowych (zasada Arrange–Act–Assert)

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -13,27 +13,68 @@ import static org.junit.Assert.*;
 public class BinarySearchTest {
 
     @Test public void elementInSequenceLength1() {
-        assertTrue(BinarySearch.search(1, new int[] {1}).isFound());
+
+        int[] sequence = {1};
+        int element = 1;
+
+        SearchResult result = BinarySearch.search(element, sequence);
+
+        assertTrue(result.isFound());
     }
 
     @Test public void elementNotInSequenceLength1() {
-        assertFalse(BinarySearch.search(0, new int[] {1}).isFound());
+
+        int[] sequence = {1};
+        int element = 0;
+
+        SearchResult result = BinarySearch.search(element, sequence);
+
+        assertFalse(result.isFound());
+
     }
 
     @Test public void elementFirstInSequence() {
-        assertEquals(1, BinarySearch.search(1, new int[] {1, 2, 3}).getPosition());
+
+        int[] sequence = {1, 2, 3};
+        int element = 1;
+
+        SearchResult result = BinarySearch.search(element, sequence);
+
+        assertEquals(1, result.getPosition());
+
     }
 
     @Test public void elementLastInSequence() {
-        assertEquals(3, BinarySearch.search(3, new int[] {1, 2, 3}).getPosition());
+
+        int[] sequence = {1, 2, 3};
+        int element = 3;
+
+        SearchResult result = BinarySearch.search(element, sequence);
+
+        assertEquals(3, result.getPosition());
+
     }
 
     @Test public void elementInMiddleOfSequence() {
-        assertEquals(2, BinarySearch.search(2, new int[] {1, 2, 3}).getPosition());
+
+        int[] sequence = {1, 2, 3};
+        int element = 2;
+
+        SearchResult result = BinarySearch.search(element, sequence);
+
+        assertEquals(2, result.getPosition());
+
     }
 
     @Test public void elementNotInSequence() {
-        assertFalse(BinarySearch.search(4, new int[] {1, 2, 3}).isFound());
+
+        int[] sequence = {1};
+        int element = 4;
+
+        SearchResult result = BinarySearch.search(element, sequence);
+
+        assertFalse(result.isFound());
+
     }
 
 }
